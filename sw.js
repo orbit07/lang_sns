16a294d3f779574362dde08e80b57c633b0dea62
orbit07 <anzunden.1@gmail.com>
2025-11-03 12:43:23 +0900

    一旦完成

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = "myspeak-sns-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k!==CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+self.addEventListener("fetch", (e) => {
+  const req = e.request;
+  // まずキャッシュ、なければネット
+  e.respondWith(
+    caches.match(req).then(res => res || fetch(req).catch(()=> caches.match("./index.html")))
+  );
+});
